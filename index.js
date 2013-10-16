d01c0fa0c91c765e0c36239bc06abcf95b7bdffa
Thomas Parisot <thomas.parisot@bbc.co.uk>
2013-10-16 14:34:41 +0100

    Providing Strategy elements

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict';
 
 var data = require('./lib/data.js');
+var strategy = require('./lib/strategy.js');
 
 module.exports = {
   getRaw: data.getRaw,
-  getJSON: data.getJSON
+  getJSON: data.getJSON,
+  getStrategyElement: strategy.getElement
 };
